140522ba49c0b6a6caa473ead95e399484db29b8
eugeneandrienko <evg.andrienko@gmail.com>
2022-04-25 20:34:06 +0300

    Initial commit

---
+++ app/src/main/java/com/eugene_andrienko/telepodcast/Telegram.java
@@ -0,0 +1,23 @@
+package com.eugene_andrienko.telepodcast;
+
+import java.io.File;
+
+
+public class Telegram
+{
+    public void login()
+    {
+    }
+
+    public void logout()
+    {
+    }
+
+    public void sendMessage(String message)
+    {
+    }
+
+    public void sendFile(File file)
+    {
+    }
+}
