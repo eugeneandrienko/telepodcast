3d3b01c022973d6d0722f29cb7c32cf48453e506
eugeneandrienko <evg.andrienko@gmail.com>
2022-05-01 12:02:02 +0300

    Make app project modular

--- app/src/main/java/com/eugene_andrienko/telepodcast/Telegram.java
+++ app/src/main/java/com/eugene_andrienko/telepodcast/Telegram.java
@@ -3,6 +3,11 @@ package com.eugene_andrienko.telepodcast;
 import java.io.File;
 
 
+/**
+ * Sends files and messages to "Saved messages" chat in Telegram.
+ *
+ * Also hides complexity of TDLib inside.
+ */
 public class Telegram
 {
     public void login()
---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module telepodcast.app {
+    requires org.slf4j;
+    requires org.slf4j.simple;
+    requires com.beust.jcommander;
+}
